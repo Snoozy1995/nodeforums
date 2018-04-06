fc49d857546c34a995bf1babd35bebb481cdd8f7
unknown <snoozy1995@gmail.com>
2018-04-06 14:36:17 +0200

    Bunch of updates, basic comments and discussions can now be made.

--- host.js
+++ host.js
@@ -17,6 +17,7 @@ const   session         = require('express-session');
 const   MongoDBStore    = require('connect-mongodb-session')(session);
 const   ObjectId        = require('mongodb').ObjectId; 
 const   io              = require('socket.io')(http);
+const   moment          = require("moment");
 const   fs              = require("fs");
 const   crypto          = require("crypto");
 var sharedsession = require("express-socket.io-session");
@@ -65,9 +66,12 @@ function connectToMongo(){
 }
 connectToMongo();
 
-function getCollectionArray(collection,next,sortby={}){
+function getCollectionArray(collection,next,options={}){
   if(!collection||!next||!global||!global.db) return;
-  global.db.collection(collection).find(sortby).toArray((err, res)=>documentHandle(err,res,next));
+  var res=global.db.collection(collection).find(options.findBy);
+  if(options.sortBy){ res.sort(options.sortBy); }
+  if(options.limit){ res.limit(options.limit); }
+  res.toArray((err, res)=>documentHandle(err,res,next));
 }
 function insertDocument(collection,insert,next){
   if(insert instanceof Object) return global.db.collection(collection).insertOne(insert,(err, res)=>documentHandle(err,res,next));
@@ -133,13 +137,13 @@ function userController(socket){
     getCollectionArray("users",(res)=>{
       if(!res.length) return socket.emit("loginError","The information provided isn't found in our database. Please check your input and try again.");
       return this.loadUser(username,true,true);
-    },{username:username,password:this.encrypt(password)});
+    },{findBy:{username:username,password:this.encrypt(password)}});
   });
   this.socket.on("register",(username,password,email)=>{
     if(!is_valid_email(email)) return; //Not valid email
     if(!password.length) return;
     if(!username.length) return; //Not valid username
-    insertDocument("users",{username:username,password:this.encrypt(password),email:email,groups:[getDefaultGroup()._id]},(res)=>{
+    insertDocument("users",{username:username,password:this.encrypt(password),email:email,groups:[getDefaultGroup()._id.toString()]},(res)=>{
       if(!res||!res.ops.length) return;
       return this.loadUser(username,true,true);
     });
@@ -148,26 +152,38 @@ function userController(socket){
     getCollectionArray("users",(res)=>{
       if(!res.length) return socket.emit("usernameAvailable",true);
       return socket.emit("usernameAvailable",false);
-    },{username:username});
+    },{findBy:{username:username}});
   });
   this.socket.on("emailAvailable",(email)=>{
     getCollectionArray("users",(res)=>{
       if(!res.length) return socket.emit("emailAvailable",true);
       return socket.emit("emailAvailable",false);
-    },{email:email});
+    },{findBy:{email:email}});
   });
 
-  //Permission groups*
-  //Permissions*
-  //*to be implemented
+  this.socket.on("createcomment",(disc,comt)=>this.createComment(disc,comt));
+  this.socket.on("creatediscussion",(directory,name,text)=>this.createDiscussion(directory,name,text));
+  this.socket.on("likecomment",(comment)=>{});
+  this.socket.on("likediscussion",(discussion)=>{});
 
   this.update_ui={};
-  this.update_pending={};
   this.socket.on("register ui",(ui,target)=>{
     this.update_ui[ui]=target;
     if(!this.updateUI(ui)) return;
   });
   this.socket.on("disconnect",()=>this.disconnect());
+}
+userController.prototype.createDiscussion=function(directory,name,text){
+  insertDocument("discussions",{directory:ObjectId(directory),name:name,text:text,created:moment().format(),author:this.session._id},()=>{
+    this.socket.emit("redirect","/"+directory);
+  });
+  return this;
+}
+userController.prototype.createComment=function(discussion,comment,parent){
+  var insert={discussion:ObjectId(discussion),text:comment,created:moment().format(),author:this.session._id};
+  if(parent){ insert.parent=parent; }
+  insertDocument("discussions",insert,(res)=>{ this.updateUIALL(); });
+  return this;
 }
 userController.prototype.disconnect=function(){
   clients.splice(this,1);
@@ -184,7 +200,7 @@ userController.prototype.loadUser=function(username,setSession=false,redirect=fa
     this.getPermissionsTree();
     //this.permissions=res[0].permissions;
     if(redirect){ setTimeout(()=>this.socket.emit("redirect","/"),500); }
-  },{username:username});
+  },{findBy:{username:username}});
 }
 userController.prototype.setSession=function(session={}){
   for(var index in session){ this.session[index]=session[index]; }
@@ -197,9 +213,24 @@ userController.prototype.encrypt=function(password){
   hash.update(password.substring(16,48));
   return hash.digest('hex');
 }
+userController.prototype.updateUIALL=function(){
+  for(var key in this.update_ui){
+    this.updateUI(key);
+  }
+}
 userController.prototype.updateUI=function(ui){
   var res=this;
   switch(ui){
+    case "discussionView":
+      console.log(res.socket.handshake.query.id);
+      return getCollectionArray("discussions",(result)=>{
+        if(!result.length) return console.log("This should be handled error getting discussionView @ userController.updateUI");
+        new discussionTree(result[0],{
+          next:(that)=>app.render("api/"+ui,{permissions:res.permissionsTree,session:res.session,discussion:that.discussion,comments:that.commentsArray},(err,html)=>res.renderUI(ui,html,err)),
+          //permissions:res.permissionsTree
+
+        });
+      },{findBy:{_id:ObjectId(res.socket.handshake.query.id)}});
     case "directoryView":
       return getCollectionArray("directories",(directories)=>{
         new directoryTree(directories,{
@@ -210,7 +241,7 @@ userController.prototype.updateUI=function(ui){
     case "childView":
       return getCollectionArray("directories",(directories)=>{
         new directoryTree(directories,{
-          next:(that)=>app.render("api/"+ui,{permissions:res.permissionsTree,session:res.session,directories:that.directories},(err,html)=>res.renderUI(ui,html,err)),
+          next:(that)=>app.render("api/"+ui,{permissions:res.permissionsTree,session:res.session,directories:that.directories,discussions:that.discussions},(err,html)=>res.renderUI(ui,html,err)),
           loadDiscussions:true,
           permissions:res.permissionsTree,
           origin:(obj,index,array)=>{ return (obj._id.toString()==res.socket.handshake.query.id); }
@@ -243,6 +274,77 @@ userController.prototype.getPermissionsTree=function(){
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //--------------------------------------------------------------------------------------------------//
+//---------------------------------------discussions Tree-------------------------------------------//
+//--------------------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+function discussionTree(discussion,config={}){
+  if(!discussion||!discussion._id) return;
+  this.discussion=discussion;
+  //config.next -> function that will be emitted once done. Will return the directoryTree.
+  if(config.next) this.next=config.next;
+  //config.origin -> can contain first sorting parameter function for filter if wished to load from something else than upper view, for example a child directory.
+  if(config.origin) this.origin=config.origin;
+  //Contains object with keys containing ids and a default. The permissions from other than default will overrule.
+  if(config.permissions) this.permissions=config.permissions;
+  this.startTime=Date.now();
+  this.pending=0;
+  this.getComments(this.origin);
+  this.retrieveUser(this.discussion);
+  return this;
+}
+
+discussionTree.prototype.getComments=function(varx=function(obj,index,array){ return (!obj.directory); }){
+  getCollectionArray("discussions",(res)=>{
+    this.comments=res;
+    console.log(this.comments);
+    if(!this.comments.length) return this.pendingF();
+    this.commentsArray=this.comments.filter(varx);
+    this.pendingF(this.commentsArray.length);
+    for(var i=0;i<this.commentsArray.length;i++){
+      if(this.commentsArray[i].author){
+        this.retrieveUser(this.commentsArray[i]);
+        //Retrieve minimal data about author.
+      }
+      this.commentsArray[i].children=this.comments.filter((obj,index,array)=>{ 
+        if(!obj.parent) return false;
+        return (obj.parent.toString()==this.commentsArray[i]._id.toString());
+      });
+      this.pendingF();
+    }
+
+  },{findBy:{discussion:this.discussion._id}});
+}
+discussionTree.prototype.retrieveUser=function(comment){
+  this.pendingF(1);
+  getCollectionArray("users",(res)=>{
+    if(res&&res.length){
+      console.log(res);
+      comment.author=res[0];
+      this.pendingF();
+    }
+  },{findBy:{_id:ObjectId(comment.author)}});
+}
+discussionTree.prototype.permissionFilter=function(array){
+  if(this.permissions&&array.length){
+    for(var i=array.length-1;i>=0;i--){
+      var perms=this.permissions.default;
+      if(this.permissions[array[i]._id.toString()]) perms=this.permissions[array[i]._id.toString()];
+      if(!hasPermission("READ",perms)){ array.splice(i,1);}
+      if(i==0) return array;
+    }
+  }else return array;
+}
+discussionTree.prototype.pendingF=function(increase=-1){
+  this.pending+=increase;
+  if(this.pending<=0){
+    console.log("Time taken loading discussionTree:",(Date.now()-this.startTime));
+    if(this.next) this.next(this);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//--------------------------------------------------------------------------------------------------//
 //----------------------------------------directory Tree--------------------------------------------//
 //--------------------------------------------------------------------------------------------------//
 //--------------------------------------------------------------------------------------------------//
@@ -259,6 +361,7 @@ function directoryTree(directories,config={}){
 
   //Contains object with keys containing ids and a default. The permissions from other than default will overrule.
   if(config.permissions) this.permissions=config.permissions;
+
   this.startTime=Date.now();
   this.pending=0;
   this.getDirectories(this.origin);
@@ -267,16 +370,26 @@ function directoryTree(directories,config={}){
 directoryTree.prototype.getDirectories=function(varx=function(obj,index,array){ return (obj.category); }){
   this.directories=this.permissionFilter(this.directoriesX.filter(varx));
   this.pendingF(this.directories.length);
-  for(var i=0;i<this.directories.length;i++){ 
+  for(var i=0;i<this.directories.length;i++){
+    if(this.origin) this.findLatestDiscussion(this.directories[i]);
     if(this.loadDiscussions){
       this.pendingF(1);
       getCollectionArray("discussions",(res)=>{
         this.discussions=res;
         this.pendingF();
-      },{directory:this.directories[i]._id.toString()});
+      },{findBy:{directory:this.directories[i]._id},sortBy:{created:-1}});
     }
     this.getChildren(this.directories[i]); 
   }
+}
+
+//Needs some rewriting to support comments too, also needs to compare the latest comment against the latest discussion to see which is newest.
+directoryTree.prototype.findLatestDiscussion=function(directory){
+  this.pendingF(1);
+  getCollectionArray("discussions",(res)=>{
+    if(res&&res.length){ directory.latest=res[0]; }
+    this.pendingF();
+  },{findBy:{directory:directory._id},sortBy:{created:-1},limit:1});
 }
 directoryTree.prototype.permissionFilter=function(array){
   if(this.permissions&&array.length){
@@ -294,6 +407,7 @@ directoryTree.prototype.getChildren=function(category){
     category.children=children;
     this.pendingF(category.children.length);
     for(var i=0;i<category.children.length;i++){
+      this.findLatestDiscussion(category.children[i]);
       this.getChildren(category.children[i]);
     }
   }
@@ -316,9 +430,13 @@ directoryTree.prototype.pendingF=function(increase=-1){
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //Used to create the backlink from current directory.
 //Should be able to support discussions.
-function originController(id,next){
+function originController(id,next,config={}){
   this.next=next;
   this.endArray=[];
+  if(config.discussion){
+    this.endArray.unshift(id);
+    id=id.directory;
+  }
   getCollectionArray("directories",(directories)=>{
     this.directories=directories;
     this.testX(id);
@@ -344,34 +462,34 @@ originController.prototype.testX=function(id){
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //First loaded after everything else is loaded.
 function loadRoutes(){
-  app.get("/createDiscussion*",(req,res)=>{
-    if(!req.query.id||req.query.id.length!=12&&req.query.id.length!=24) return res.redirect("/");
-    getCollectionArray("directories",(directories)=>{
-      if(!directories.length) return res.redirect("/");
-      new originController(directories[0]._id,(r)=>{
-        return res.render("directory",{session:req.session,query:req.query,origin:r});
-      });
-    },{_id:ObjectId(req.query.id)});
-  });
   app.get(["/login","/register","/"],normalRoute);
   app.get("/logout",(req,res)=>{
     req.session.destroy(function(err){ if(err) return console.log(err); });
     return res.redirect("/");
   });
-  app.get(["/:id"],(req,res)=>{
+  app.get("/:id/create",(req,res)=>{
+    if(!req.params.id||req.params.id.length!=12&&req.params.id.length!=24) return res.redirect("/");
+    getCollectionArray("directories",(directories)=>{
+      if(!directories.length) return res.redirect("/");
+      new originController(directories[0]._id,(r)=>{
+        return res.render("createDiscussion",{session:req.session,params:req.params,origin:r});
+      });
+    },{findBy:{_id:ObjectId(req.params.id)}});
+  });
+  app.get("/:id",(req,res)=>{
     if(!req.params.id||req.params.id.length!=12&&req.params.id.length!=24) return res.redirect("/");
     getCollectionArray("directories",(directories)=>{
       if(!directories.length) return;
       new originController(directories[0]._id,(r)=>{
         return res.render("directory",{session:req.session,query:req.query,origin:r});
       });
-    },{_id:ObjectId(req.params.id)});
+    },{findBy:{_id:ObjectId(req.params.id)}});
     getCollectionArray("discussions",(disc)=>{
       if(!disc.length) return;
-      new originController(disc[0]._id,(r)=>{
+      new originController(disc[0],(r)=>{
         return res.render("discussion",{session:req.session,query:req.query,origin:r});
-      });
-    },{_id:ObjectId(req.params.id)});
+      },{discussion:true});
+    },{findBy:{_id:ObjectId(req.params.id)}});
   });
   app.get("/*",(req,res)=>res.redirect("/"));
   http.listen(80);
